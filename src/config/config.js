5ed18499d089a3e62873d93bd83bdf893758404c
mdridoy-khan <mdridoy9902@gmail.com>
2025-11-24 10:24:19 +0600

    base url changed

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,4 @@
 // export const API_BASE_URL = "https://quiz.kitchzone.com/api";
-export const API_BASE_URL = "https://quizcompetitionbackend.onrender.com/api";
+// export const API_BASE_URL = "https://quizcompetitionbackend.onrender.com/api";
+export const API_BASE_URL = "https://backend.quizen.online/api";
 // export const API_BASE_URL = "http://192.168.0.238:8011/api";
